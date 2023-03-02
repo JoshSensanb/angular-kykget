f150aed1a9eff8be429f0647988396f4d0fa729a
JoshSensanb <111921798+JoshSensanb@users.noreply.github.com>
2023-03-02 13:06:16 -0500

    2 files modified, 3 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,12 @@
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
